4f2ee40bc22549c54d0c801fb319ac129e67ad28
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-04-14 21:10:17 +0800

    support convert scss

--- document-management-converter/server.js
+++ document-management-converter/server.js
@@ -4,5 +4,6 @@ cp.exec("npm run convert", (error, stdout) => {
     console.error(error);
     return;
   }
-  console.log(stdout)
+  console.log(stdout);
+  console.log("Successfully compiled sass files.\n")
 })
