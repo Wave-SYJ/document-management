47de6f9cb27a83e6ad1cd309dd92d635b64f3dcd
TzRain <2446078648@qq.com>
2021-05-15 01:16:40 +0800

    实现词条service模块

---
+++ document-management-web/src/main/java/cn/edu/seu/cose/docmanage/mapper/EntryMapper.java
@@ -0,0 +1,16 @@
+package cn.edu.seu.cose.docmanage.mapper;
+
+import cn.edu.seu.cose.docmanage.entity.Entry;
+import com.github.pagehelper.Page;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EntryMapper {
+    void insertEntry(Entry entry);
+    void deleteEntry(List<UUID> entryIdList);
+    void updateEntry(Entry entry);
+    Page<Entry> findEntry(String entryName);
+}
---
+++ document-management-web/src/main/java/cn/edu/seu/cose/docmanage/service/EntryService.java
@@ -0,0 +1,49 @@
+package cn.edu.seu.cose.docmanage.service;
+
+
+import cn.edu.seu.cose.docmanage.entity.Entry;
+import cn.edu.seu.cose.docmanage.entity.User;
+import cn.edu.seu.cose.docmanage.exception.SimpleException;
+import cn.edu.seu.cose.docmanage.mapper.EntryMapper;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EntryService {
+
+    @Autowired
+    private EntryMapper entryMapper;
+
+    public void insertEntry(Entry entry){
+        if(entry.getId()==null){
+            entry.setId(UUID.randomUUID());
+        }
+        if(!StringUtils.hasText(entry.getName())){
+            throw new SimpleException("词条名称不能为空");
+        }
+        if(!StringUtils.hasText(entry.getDescription())){
+            throw new SimpleException("词条描述不能为空");
+        }
+        entryMapper.insertEntry(entry);
+    }
+
+    public void deleteEntry(List<UUID> entryIdList){
+        entryMapper.deleteEntry(entryIdList);
+    }
+
+    public void updateEntry(Entry entry){
+        if(entry==null)throw new SimpleException("词条内容为空");
+        entryMapper.updateEntry(entry);
+    }
+
+    public Page<Entry> findEntry(int pageNum, int pageSize, String entryName) {
+        PageHelper.startPage(pageNum, pageSize);
+        return entryMapper.findEntry(entryName);
+    }
+}
