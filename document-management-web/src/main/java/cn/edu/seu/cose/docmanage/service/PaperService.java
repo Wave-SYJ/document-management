94bd5b22892ed67a3691b06e805745f3c4d332be
书断言 <2104876017@qq.com>
2021-05-15 00:43:50 +0800

    paper class PaperMapper mapper PaperService service

--- document-management-web/src/main/java/cn/edu/seu/cose/docmanage/service/PaperService.java
+++ document-management-web/src/main/java/cn/edu/seu/cose/docmanage/service/PaperService.java
@@ -2,10 +2,13 @@ package cn.edu.seu.cose.docmanage.service;
 
 import cn.edu.seu.cose.docmanage.entity.Paper;
 import cn.edu.seu.cose.docmanage.mapper.PaperMapper;
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
+import cn.edu.seu.cose.docmanage.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PaperService {
@@ -13,9 +16,24 @@ public class PaperService {
     @Autowired
     private PaperMapper paperMapper;
 
-    public Page<Paper> findPaperPage(int pageNum, int pageSize, String searchKey, String searchValue) {
-        PageHelper.startPage(pageNum, pageSize);
-        return paperMapper.findPaperPage(searchKey, searchValue);
+    @Transactional
+    public void insetPaper(Paper paper){
+        if(paper.getId()==null)
+            paper.setId(UUID.randomUUID());
+
+        paperMapper.insertPaper(paper);
+
+    }
+
+
+    public void deleteAllPapers(List<UUID> paperIds){
+        paperMapper.deletePapers(paperIds);
+    }
+
+    public void updatePaper(Paper paper){
+        if(paper==null)
+            return;
+        paperMapper.updatePaper(paper);
     }
 
 }
