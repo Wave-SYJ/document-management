524154c2d64618e4b36b0d7a967684c7cb83d993
书断言 <2104876017@qq.com>
2021-05-15 00:33:48 +0800

    paper class

---
+++ document-management-web/src/main/java/cn/edu/seu/cose/docmanage/entity/Paper.java
@@ -0,0 +1,25 @@
+package cn.edu.seu.cose.docmanage.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.servlet.tags.form.TextareaTag;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paper {
+    private UUID id;
+    private String title;
+    private String author;
+    private String paperAbstract;
+    private Date publishTime;
+    private String authorOffice;
+    private String keyword;
+    private UUID journalId;
+
+
+}
