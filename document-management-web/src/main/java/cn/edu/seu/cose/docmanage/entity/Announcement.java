84d7c8128e6a2c185eeaacbac4f6090e1dbb4d1f
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-05-23 19:23:46 +0800

    push

--- document-management-web/src/main/java/cn/edu/seu/cose/docmanage/entity/Announcement.java
+++ document-management-web/src/main/java/cn/edu/seu/cose/docmanage/entity/Announcement.java
@@ -3,6 +3,7 @@ package cn.edu.seu.cose.docmanage.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.UUID;
@@ -14,5 +15,7 @@ public class Announcement {
     private UUID id;
     private String content;
     private UUID publisherId;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date publishTime;
 }
