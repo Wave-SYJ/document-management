cbfaea8fa4cf96bd71d38fc72c853fa4bf982536
li_ruii <li15152980297@163.com>
2021-05-23 15:39:25 +0800

    Journal

---
+++ document-management-web/src/main/java/cn/edu/seu/cose/docmanage/mapper/SubscriptionMapper.java
@@ -0,0 +1,15 @@
+package cn.edu.seu.cose.docmanage.mapper;
+
+import cn.edu.seu.cose.docmanage.entity.Journal;
+import com.github.pagehelper.Page;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SubscriptionMapper {
+    void insertJournal(Journal journal);
+    void deleteJournals(List<UUID> journalIds);
+    void updateJournal(Journal newJournal);
+    Page<Journal> findJournalPage(String searchKey, String searchValue);
+}
