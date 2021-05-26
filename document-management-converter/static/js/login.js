992c6a5aa3a7bf4df075b441200913044bd80152
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-05-26 20:38:53 +0800

    change verify text

--- document-management-converter/static/js/login.js
+++ document-management-converter/static/js/login.js
@@ -1,8 +1,8 @@
 $('#login-form')
   .form({
     fields: {
-      username: 'empty',
-      password: 'empty'
+      username: { rules: [{ type: 'empty', prompt: '用户名不能为空' }] },
+      password: { rules: [{ type: 'empty', prompt: '密码不能为空' }] }
     }
   })
   ;
